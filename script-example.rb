23b2281242d4ba09e2ab049bd029db8ead5c2a33
Thomas Pierson <web.pierson@gmail.com>
2012-01-18 21:20:43 +0100

    adaptation of the script with the new interface

--- script-example.rb
+++ script-example.rb
@@ -5,5 +5,6 @@ require 'css2less'
 css = File.read(ARGV[0])
 
 converter = Css2Less::Converter.new(css)
-converter.generate
-converter.print
+converter.generate_tree
+converter.render_less
+puts converter.get_less
