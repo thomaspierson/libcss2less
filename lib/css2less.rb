d0884fef8ac1fb7fa95c0b15cfb168e0dc886397
Thomas Pierson <contact@thomaspierson.fr>
2012-04-23 22:29:54 +0200

    set gpl-3 licence on the project

--- lib/css2less.rb
+++ lib/css2less.rb
@@ -1,3 +1,20 @@
+# Copyright 2012 Thomas Pierson <contact@thomaspierson.fr> ,
+#		 Marcin Kulik <m@ku1ik.com>
+#
+# This file is part of Css2Less Library.
+#
+# Css2Less Library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Css2Less Library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 module Css2Less
   
