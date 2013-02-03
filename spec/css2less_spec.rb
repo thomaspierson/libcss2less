fd29a36aa7964c4bd755a58b0c3be4902fc91df9
Thomas Pierson <contact@thomaspierson.fr>
2013-02-03 12:39:59 +0100

    implement new rspec test: 'should convert basic css structure into less structure'

--- spec/css2less_spec.rb
+++ spec/css2less_spec.rb
@@ -6,3 +6,29 @@ describe Css2Less do
     subject.const_get('VERSION').should_not be_empty
   end
 end
+
+describe Css2Less::Converter do
+  it "should convert basic css structure into less structure" do
+    css = <<EOF
+#hello {
+    color: blue;
+}
+
+#hello #buddy {
+    background: red;
+}
+EOF
+    less = <<EOF
+#hello {
+    color: blue;
+    #buddy {
+        background: red;
+    }
+}
+EOF
+    converter = Css2Less::Converter.new(css)
+    converter.process_less
+    converter.get_less.should eq(less)
+  end
+
+end
